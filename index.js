48ed48bee7fe5ca8698a294952b2b97a53e3e8f0
GauravRasal07 <gauravvr.77@gmail.com>
2021-10-02 22:43:25 +0530

    created the index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const flash = require("connect-flash");
+const expressSanitizer = require("express-sanitizer");
+const methodOverRide = require("method-override");
+const dotenv = require("dotenv").config();
+const app = express();
+
+mongoose
+  .connect(process.env.DBURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to Locly Database!"))
+  .catch((error) => console.log(error.message));
+
+app.use(
+  require("express-session")({
+    secret: process.env.secret,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(expressSanitizer());
+
+app.use(flash());
+
+app.use(express.static(__dirname + "/public"));
+
+app.use(methodOverRide("_method"));
+
+// passport.use(new localStrategy(User.authenticate()));
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("The Server is Listening!!!");
+});
